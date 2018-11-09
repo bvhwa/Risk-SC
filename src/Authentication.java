24ca4c27631249559db5e779a2543666f86e5ad1
Nilay Pachauri <pachauri@usc.edu>
2018-11-08 22:01:19 -0800

    Added Hash Function Similar to HashCode for Strings in Default Java Library

---
+++ src/Authentication.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
+public class Authentication {
+
+	public static String hashString(String string)	{
+
+		BigInteger value = new BigInteger("0");
+		
+		for (int i = 0; i < string.length(); i++)
+			value.add(new BigInteger(Double.toString(string.charAt(i)*Math.pow(31, string.length() - (i + 1)))));
+		
+		return value.toString();
+	}
+	
+}
