e700e68eba5455096ab76db3a3a5e44e454588bf
Nilay Pachauri <pachauri@usc.edu>
2018-11-09 01:31:08 -0800

    Added a list of countries

--- src/Adjacencies.java
+++ src/Adjacencies.java
@@ -1,9 +1,58 @@
 
 public abstract class Adjacencies {
 	/*
+	 * List of Territories
+	 */
+	private String territories[] = {
+			"Lyon Center",						//  0
+			"Dedeaux Field",					//  1
+			"Marks Tennis Stadium",				//  2
+			"Little Galen Center",				//  3
+			"John McKay Center",				//  4
+			"Cromwell Field",					//  5
+			
+			"Kaprielian Hall",					//  6
+			"Seeley G. Mudd",					//  7
+			"Stauffer Hall",					//  8
+			"Zumberge Hall",					//  9
+			"Hedco Neurosciences Building",		// 10
+			"Michelson Hall",					// 11
+			"Seaver Science Library",			// 12
+			
+			"Salvatori Computer Science",		// 13
+			"Vivian Hall",						// 14
+			"Tutor Hall",						// 15
+			"Olin Hall",						// 16
+			"Epstein Family Plaza",				// 17
+			
+			"Fertita Hall",						// 18
+			"Popovich Hall",					// 19
+			"Dauterive Hall",					// 20
+			"Hoffman Hall",						// 21
+			"Leventhal School of Accounting",	// 22
+			
+			"SCA",								// 23
+			"SCB",								// 24
+			"Norris Cinema Theatre",			// 25
+			
+			"Tutor Campus Center",				// 26
+			"Taper Hall",						// 27
+			"Von KleinSmid Center",				// 28
+			"Leavey Library",					// 29
+			"Doheny Library",					// 30
+			"Alumni Park",						// 31
+			"McCarthy Quad",					// 32
+			"Tommy Trojan",						// 33
+			
+			"Annenberg Hall",					// 34
+			"Annenberg School for Communication" //35
+	};
+	
+	
+	/*
 	 * Athletics Territories
 	 */
-	private String lyonCenter[];
+	private String lyonCenter[] = {"Dedeaux Field", };
 	private String dedeauxField[];
 	private String marksTennisStadium[];
 	private String littleGalen[];
