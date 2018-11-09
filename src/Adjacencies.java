c3c52318102a1659179e30de27fce2f1515d8846
Nilay Pachauri <pachauri@usc.edu>
2018-11-09 01:08:23 -0800

    Created Framework for Utility Class Adjacencies with Comments

---
+++ src/Adjacencies.java
@@ -0,0 +1,65 @@
+
+public abstract class Adjacencies {
+	/*
+	 * Athletics Territories
+	 */
+	private String lyonCenter[];
+	private String dedeauxField[];
+	private String marksTennisStadium[];
+	private String littleGalen[];
+	private String mcKayCenter[];
+	private String cromwellField[];
+
+	/*
+	 * Dornsife STEM Territories
+	 */
+	private String kaprilean[];
+	private String seeleyMudd[];
+	private String stauffer[];
+	private String zumberge[];
+	private String hedcoNeuro[];
+	private String michelsonHall[];
+
+	/*
+	 * Viterbi Territories
+	 */
+	private String salvatori[];
+	private String vivian[];
+	private String tutorHall[];
+	private String olin[];
+	private String powell[];
+
+	/*
+	 * Marshall Territories
+	 */
+	private String fertitaHall[];
+	private String popovich[];
+	private String hoffman[];
+	private String leventhal[];
+	private String dauteriveHall[];
+
+	/*
+	 * Cinematic Arts Territories
+	 */
+	private String sca[];
+	private String scb[];
+	private String norrisCinema[];
+
+	/*
+	 * Dornsife LAC Territories
+	 */
+	private String tutorCenter[];
+	private String taper[];
+	private String vonKleinsmid[];
+	private String leaveyLibrary[];
+	private String dohenyLibrary[];
+	private String alumniPark[];
+	private String mcCarthyQuad[];
+	private String tommyTrojan[];
+
+	/*
+	 * Annenberg Territories
+	 */
+	private String wallisAnnenbergHall[];
+	private String annenbergSchool[];
+}
