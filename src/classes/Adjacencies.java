56d055cb1e53b4ca3548b930137abed2f98ce6bb
Nilay Pachauri <pachauri@usc.edu>
2018-11-14 19:32:17 -0800

    Finished adding all the dependencies

--- src/classes/Adjacencies.java
+++ src/classes/Adjacencies.java
@@ -19,18 +19,18 @@ public abstract class Adjacencies {
 			"Stauffer Hall",					// 10
 			"Zumberge Hall",					// 11
 
-			"Olin Hall",						// 13
-			"Tutor Hall",						// 14
-			"Epstein Family Plaza",				// 15
-			"Vivian Hall",						// 16
-			"Salvatori Computer Science",		// 17
-			"Seaver Science Library",			// 18
+			"Olin Hall",						// 12
+			"Tutor Hall",						// 13
+			"Epstein Family Plaza",				// 14
+			"Vivian Hall",						// 15
+			"Salvatori Computer Science",		// 16
+			"Seaver Science Library",			// 17
 
-			"Leventhal School of Accounting",	// 19
-			"Hoffman Hall",						// 20
-			"Dauterive Hall",					// 21
-			"Popovich Hall",					// 22
-			"Fertita Hall",						// 23
+			"Leventhal School of Accounting",	// 18
+			"Hoffman Hall",						// 19
+			"Dauterive Hall",					// 20
+			"Popovich Hall",					// 21
+			"Fertita Hall",						// 22
 
 			"Norris Cinema Theatre",			// 23
 			"SCB",								// 24
@@ -64,53 +64,53 @@ public abstract class Adjacencies {
 	 * Dornsife STEM Territories
 	 */
 	private int kaprilean[] = {4, 7};
-	private int michelsonHall[] = {4, 6, 8, 13};
-	private int seeleyMudd[] = {5, 7, 9, 13};
-	private int hedcoNeuro[] = {5, 8, 10, 16, 34};
-	private int stauffer[] = {9, 11, 16, 29};
-	private int zumberge[] = {10, 19};
+	private int michelsonHall[] = {4, 6, 8, 12};
+	private int seeleyMudd[] = {5, 7, 9, 12};
+	private int hedcoNeuro[] = {5, 8, 10, 15, 34};
+	private int stauffer[] = {9, 11, 15, 29};
+	private int zumberge[] = {10, 18};
 
 	/*
 	 * Viterbi Territories
 	 */
-	private int salvatori[] = {};
-	private int vivian[] = {};
-	private int tutorHall[] = {};
-	private int olin[] = {};
-	private int eQuad[] = {};
-	private int seaverScienceLibrary[] = {};
+	private int olin[] = {7, 8, 13, 14, 15};
+	private int tutorHall[] = {12, 14, 16};
+	private int eQuad[] = {12, 13, 15, 16, 17};
+	private int vivian[] = {9, 10, 12, 14, 17};
+	private int salvatori[] = {13, 14, 17};
+	private int seaverScienceLibrary[] = {14, 15, 16};
 
 	/*
 	 * Marshall Territories
 	 */
-	private int fertitaHall[] = {};
-	private int popovich[] = {};
-	private int hoffman[] = {};
-	private int leventhal[] = {};
-	private int dauteriveHall[] = {};
+	private int leventhal[] = {11, 19, 20, 29, 30};
+	private int hoffman[] = {18, 20};
+	private int dauteriveHall[] = {18, 19, 21, 30, 33};
+	private int popovich[] = {20,22};
+	private int fertitaHall[] = {21};
 
 	/*
 	 * Cinematic Arts Territories
 	 */
-	private int sca[] = {};
-	private int scb[] = {};
-	private int norrisCinema[] = {};
+	private int norrisCinema[] = {24, 25};
+	private int scb[] = {0, 2, 23, 25};
+	private int sca[] = {3, 23, 24, 35};
 
 	/*
 	 * Dornsife LAC Territories
 	 */
-	private int tutorCenter[] = {};
-	private int taper[] = {};
-	private int vonKleinsmid[] = {};
-	private int leaveyLibrary[] = {};
-	private int dohenyLibrary[] = {};
-	private int alumniPark[] = {};
-	private int mcCarthyQuad[] = {};
-	private int tommyTrojan[] = {};
+	private int taper[] = {27, 28, 35};
+	private int vonKleinsmid[] = {26, 28, 31, 32, 35};
+	private int leaveyLibrary[] = {26, 27, 32};
+	private int tutorCenter[] = {10, 18, 30, 34};
+	private int tommyTrojan[] = {18, 20, 29, 31, 34};
+	private int alumniPark[] = {27, 30, 32, 33};
+	private int mcCarthyQuad[] = {27, 28, 31, 33};
+	private int dohenyLibrary[] = {20, 31, 32};
 
 	/*
 	 * Annenberg Territories
 	 */
-	private int wallisAnnenbergHall[] = {};
-	private int annenbergSchool[] = {};
+	private int wallisAnnenbergHall[] = {3, 5, 9, 29, 35};
+	private int annenbergSchool[] = {3, 25, 26, 27, 34};
 }
