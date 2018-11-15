08b00f0545d64cdfd2cd115a3ea444c87143c50f
Nilay Pachauri <pachauri@usc.edu>
2018-11-15 15:49:52 -0800

    Added Comment code for the User class

--- src/classes/User.java
+++ src/classes/User.java
@@ -41,66 +41,131 @@ public class User {
 	/*
 	 * Getters and Setters for the Private Instance Variables
 	 */
+	
+	/**
+	 * @return first name of the user
+	 */
 	public String getFirstName() {
 		return firstName;
 	}
 
+	/**
+	 * 
+	 * @param firstName the first name to set
+	 */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
+	/**
+	 * 
+	 * @return last name of the user
+	 */
 	public String getLastName() {
 		return lastName;
 	}
 
+	/**
+	 * 
+	 * @param lastName the last name to set
+	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
+	
+	/**
+	 * 
+	 * @return user name of the user
+	 */
 	public String getUserName() {
 		return userName;
 	}
 
+	
+	/**
+	 * 
+	 * @param userName the user name to set
+	 */
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
 
+	/**
+	 * 
+	 * @return image of the user
+	 */
 	public String getImage() {
 		return image;
 	}
 
+	/**
+	 * 
+	 * @param image the image to set
+	 */
 	public void setImage(String image) {
 		this.image = image;
 	}
 
+	/**
+	 * 
+	 * @return if the user is a host
+	 */
 	public boolean isHost() {
 		return isHost;
 	}
 
+	/**
+	 * 
+	 * @param isHost set whether the user is a host
+	 */
 	public void setHost(boolean isHost) {
 		this.isHost = isHost;
 	}
 
+	/**
+	 * 
+	 * @return the number of territories held by the user
+	 */
 	public int getTerritories() {
 		return territories;
 	}
 
+	/**
+	 * 
+	 * @param territories the amount of territories to set
+	 */
 	public void setTerritories(int territories) {
 		this.territories = territories;
 	}
 
+	/**
+	 * 
+	 * @return the number of troops held by the user
+	 */
 	public int getTroops() {
 		return troops;
 	}
 
+	/**
+	 * 
+	 * @param troops the amount of troops to set
+	 */
 	public void setTroops(int troops) {
 		this.troops = troops;
 	}
 
+	
+	/**
+	 * Increment the amount of territories held by the user
+	 */
 	public void addTerritory() {
 		this.territories++;
 	}
 	
+	/**
+	 * Decrement the amount of territories held by the user
+	 */
 	public void removeTerritory()	{
 		this.territories--;
 	}
