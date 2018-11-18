ddd0bb8f25d77a41e2d311edf691a498b61b8109
Nilay Pachauri <pachauri@usc.edu>
2018-11-18 05:25:48 -0800

    Removed Unnecessary Comment Code

--- src/server/WaitingRoom.java
+++ src/server/WaitingRoom.java
@@ -103,26 +103,4 @@ public class WaitingRoom {
 	public void error (Throwable error)	{
 		
 	}
-	
-//	// private int numUsers = 0;
-//	static Set<Session> players = Collections.synchronizedSet(new HashSet<Session>());
-//	
-//	@OnOpen
-//	public void onOpen(Session user) throws Exception {
-//		// System.out.println("Open connection...");
-//		players.add(user);
-//		for(Session player: players) {
-//			player.getBasicRemote().sendObject(player.getId());
-//		}
-//	}
-//
-//	@OnClose
-//	public void onClose(Session user) throws Exception {
-//		players.remove(user);
-//		for(Session player: players) {
-//			if(!user.getId().equals(player.getId())) {
-//				player.getBasicRemote().sendObject(player.getId());
-//			}
-//		}
-//	}
 }
