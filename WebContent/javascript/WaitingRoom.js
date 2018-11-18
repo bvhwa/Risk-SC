47558a28c6b46fff41da031295d4bb611cb81256
Nilay Pachauri <pachauri@usc.edu>
2018-11-18 04:01:38 -0800

    Removed Alert Messages

--- WebContent/javascript/WaitingRoom.js
+++ WebContent/javascript/WaitingRoom.js
@@ -10,19 +10,11 @@ function connectToServer()	{
 	socket.onmessage = function(event)	{
 		var usernames = event.data.split("&");
 		
-		alert(event.data);
-		alert(usernames);
-		
 		for (var i = 1; i <= 4; i++)	{
 			document.getElementById("wplayer" + i).innerHTML = (i <= usernames.length) ?  usernames[i - 1] : "Waiting for players";
 		}
 	}
 	socket.onclose = function(event)	{
-//		document.getElementById("mychat").innerHTML += "Disconnected!<br />";
+		
 	}
-}
-
-function sendMessage()	{
-	socket.send("Michael: " + document.chatform.message.value);
-	return false;
 }
