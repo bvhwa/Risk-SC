3e75eba34accfe43b52721ed569a3064dc7ad1f9
Nilay Pachauri <pachauri@usc.edu>
2018-11-18 02:34:06 -0800

    Pushing updates on the filenames and updates to WaitingRoom WebSockets

---
+++ WebContent/javascript/WaitingRoom.js
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+function connectToServer()	{
+	socket = new WebSocket("ws://localhost:8080/Risk-SC/ss");
+	socket.onopen = function(event)	{
+		socket.send(sessionStorage.getItem("username"));
+	}
+	socket.onmessage = function(event)	{
+		var usernames = event.data.split("%");
+		
+		for (var i = 1; i <= 4; i++)	{
+			document.getElementById("wplayer" + i).innerHTML = (i <= usernames.length) ?  usernames[i] : "Waiting for players";
+		}
+	}
+	socket.onclose = function(event)	{
+		document.getElementById("mychat").innerHTML += "Disconnected!<br />";
+	}
+}
+
+function sendMessage()	{
+	socket.send("Michael: " + document.chatform.message.value);
+	return false;
+}
--- src/server/WaitingRoomWS.java
+++ src/server/WaitingRoomWS.java
@@ -1,36 +1,94 @@
 package server;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Vector;
 
 import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
-@ServerEndpoint("/endpoint")
+@ServerEndpoint(value="/ss")
 public class WaitingRoomWS {
 	
-	// private int numUsers = 0;
-	static Set<Session> players = Collections.synchronizedSet(new HashSet<Session>());
+	private static Vector <Session> players = new Vector<Session>();
+	private static HashMap <Session, String> usernames = new HashMap <Session, String>();
 	
 	@OnOpen
-	public void onOpen(Session user) throws Exception {
-		// System.out.println("Open connection...");
-		players.add(user);
-		for(Session player: players) {
-			player.getBasicRemote().sendObject(player.getId());
-		}
+	public void open(Session session)	{
+		System.out.println("Connection!");
+		this.players.addElement(session);
 	}
-
-	@OnClose
-	public void onClose(Session user) throws Exception {
-		players.remove(user);
-		for(Session player: players) {
-			if(!user.getId().equals(player.getId())) {
-				player.getBasicRemote().sendObject(player.getId());
+	
+	@OnMessage
+	public void message(String message, Session session)	{
+		System.out.println(message);
+		this.usernames.put(session, message);
+		
+		String users = "";
+		for (int i = 0; i < this.players.size(); i++)	{
+			users += this.usernames.get(this.players.get(i)) + ((i < this.players.size() - 1) ? "&" : "");
+		}
+		
+		for (Session player: this.players)	{
+			
+			try {
+				player.getBasicRemote().sendText(users);
+			} catch (IOException ioe) {
+				System.out.println("ioe: " + ioe.getMessage());
 			}
+
 		}
 	}
+	
+	@OnClose
+	public void close(Session session)	{
+		System.out.println("Disconnected " + this.usernames.get(session) + "!");
+		this.players.remove(session);
+		
+		String users = "";
+		for (int i = 0; i < this.players.size(); i++)	{
+			users += this.usernames.get(this.players.get(i)) + ((i < this.players.size() - 1) ? "&" : "");
+		}
+		
+		for (Session player: this.players)	{
+			
+			try {
+				player.getBasicRemote().sendText(users);
+			} catch (IOException ioe) {
+				System.out.println("ioe: " + ioe.getMessage());
+			}
+
+		}
+	}
+	
+	@OnError
+	public void error (Throwable error)	{
+		
+	}
+	
+//	// private int numUsers = 0;
+//	static Set<Session> players = Collections.synchronizedSet(new HashSet<Session>());
+//	
+//	@OnOpen
+//	public void onOpen(Session user) throws Exception {
+//		// System.out.println("Open connection...");
+//		players.add(user);
+//		for(Session player: players) {
+//			player.getBasicRemote().sendObject(player.getId());
+//		}
+//	}
+//
+//	@OnClose
+//	public void onClose(Session user) throws Exception {
+//		players.remove(user);
+//		for(Session player: players) {
+//			if(!user.getId().equals(player.getId())) {
+//				player.getBasicRemote().sendObject(player.getId());
+//			}
+//		}
+//	}
 }
